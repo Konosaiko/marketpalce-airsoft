ea7e9a3c83006819f21d9fc683cb6cbc208cfae8
Konosaiko <xkonotv@gmail.com>
2024-09-04 14:44:00 +0200

    Création du CRUD pour les catégories

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[Route('/admin/category')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'app_category_index', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $mainCategories = $categoryRepository->findBy(['parent' => null], ['name' => 'ASC']);
+        return $this->render('category/index.html.twig', [
+            'mainCategories' => $mainCategories,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = $slugger->slug($category->getName())->lower();
+            $category->setSlug($slug);
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('category/new.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = $slugger->slug($category->getName())->lower();
+            $category->setSlug($slug);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
+    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($category);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -19,18 +18,23 @@ class Category
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $description = null;
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $slug = null;
 
-    /**
-     * @var Collection<int, Listing>
-     */
-    #[ORM\ManyToMany(targetEntity: Listing::class, inversedBy: 'categories')]
-    private Collection $sell;
+    #[ORM\ManyToMany(targetEntity: Listing::class, mappedBy: 'categories')]
+    private Collection $listings;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children')]
+    #[ORM\JoinColumn(nullable: true)]
+    private ?self $parent = null;
+
+    #[ORM\OneToMany(targetEntity: self::class, mappedBy: 'parent')]
+    private Collection $children;
 
     public function __construct()
     {
-        $this->sell = new ArrayCollection();
+        $this->listings = new ArrayCollection();
+        $this->children = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -43,46 +47,89 @@ class Category
         return $this->name;
     }
 
-    public function setName(string $name): static
+    public function setName(string $name): self
     {
         $this->name = $name;
-
         return $this;
     }
 
-    public function getDescription(): ?string
+    public function getSlug(): ?string
     {
-        return $this->description;
+        return $this->slug;
     }
 
-    public function setDescription(string $description): static
+    public function setSlug(string $slug): self
     {
-        $this->description = $description;
-
+        $this->slug = $slug;
         return $this;
     }
 
     /**
      * @return Collection<int, Listing>
      */
-    public function getSell(): Collection
+    public function getListings(): Collection
     {
-        return $this->sell;
+        return $this->listings;
     }
 
-    public function addSell(Listing $sell): static
+    public function addListing(Listing $listing): self
     {
-        if (!$this->sell->contains($sell)) {
-            $this->sell->add($sell);
+        if (!$this->listings->contains($listing)) {
+            $this->listings->add($listing);
+            $listing->addCategory($this);
         }
-
         return $this;
     }
 
-    public function removeSell(Listing $sell): static
+    public function removeListing(Listing $listing): self
     {
-        $this->sell->removeElement($sell);
-
+        if ($this->listings->removeElement($listing)) {
+            $listing->removeCategory($this);
+        }
         return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): self
+    {
+        if (!$this->children->contains($child)) {
+            $this->children->add($child);
+            $child->setParent($this);
+        }
+        return $this;
+    }
+
+    public function removeChild(self $child): self
+    {
+        if ($this->children->removeElement($child)) {
+            // set the owning side to null (unless already changed)
+            if ($child->getParent() === $this) {
+                $child->setParent(null);
+            }
+        }
+        return $this;
+    }
+
+    public function isMainCategory(): bool
+    {
+        return $this->parent === null;
     }
 }
