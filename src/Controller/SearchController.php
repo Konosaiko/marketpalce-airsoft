cb43b582c32aec04946ac8c6d3436a819ed24cce
Konosaiko <xkonotv@gmail.com>
2024-09-06 12:31:06 +0200

    Création du controller pour la recherche d'annonces

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\SearchFormType;
+use App\Repository\ListingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function search(Request $request, ListingRepository $listingRepository): Response
+    {
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
+
+        $results = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $query = $form->get('query')->getData();
+            $results = $listingRepository->search($query);
+        }
+
+        return $this->render('search/index.html.twig', [
+            'form' => $form->createView(),
+            'results' => $results,
+        ]);
+    }
+}
