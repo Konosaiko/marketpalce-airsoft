66cd58d3b76578cd0cea95b3ea63c6634411c44a
Konosaiko <xkonotv@gmail.com>
2024-09-12 03:06:26 +0200

    Ajout du tri par prix et date

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,24 +11,28 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
+    #[Route('/search', name: 'app_listing_search')]
     public function search(Request $request, ListingRepository $listingRepository): Response
     {
         $form = $this->createForm(SearchFormType::class);
         $form->handleRequest($request);
 
         $results = [];
+        $sortBy = $request->query->get('sortBy', 'createdAt');
+        $sortOrder = $request->query->get('sortOrder', 'DESC');
 
         if ($form->isSubmitted() && $form->isValid()) {
             $query = $form->get('query')->getData();
             $region = $form->get('region')->getData();
             $department = $form->get('department')->getData();
-            $results = $listingRepository->search($query, $region, $department);
+            $results = $listingRepository->search($query, $region, $department, $sortBy, $sortOrder);
         }
 
         return $this->render('search/index.html.twig', [
             'form' => $form->createView(),
             'results' => $results,
+            'sortBy' => $sortBy,
+            'sortOrder' => $sortOrder,
         ]);
     }
 }
