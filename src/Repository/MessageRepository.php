b7dd9fff324dfa9ada07cefa1981b002ee213941
Konosaiko <xkonotv@gmail.com>
2024-09-21 20:45:11 +0200

    Optimisation des requêtes

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -7,9 +7,6 @@ use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Message>
- */
 class MessageRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -17,34 +14,44 @@ class MessageRepository extends ServiceEntityRepository
         parent::__construct($registry, Message::class);
     }
 
-    /**
-     * @return Message[] Returns an array of Message objects
-     */
-    public function findConversation(User $user1, User $user2): array
+    public function findUserConversations(User $user): array
     {
-        return $this->createQueryBuilder('m')
-            ->where('m.sender = :user1 AND m.recipient = :user2')
-            ->orWhere('m.sender = :user2 AND m.recipient = :user1')
-            ->setParameter('user1', $user1)
-            ->setParameter('user2', $user2)
-            ->orderBy('m.sentAt', 'ASC')
+        $qb = $this->createQueryBuilder('m');
+
+        return $qb->select('DISTINCT CASE 
+                WHEN m.sender = :user THEN IDENTITY(m.recipient)
+                ELSE IDENTITY(m.sender)
+            END AS otherUserId')
+            ->where($qb->expr()->orX(
+                $qb->expr()->eq('m.sender', ':user'),
+                $qb->expr()->eq('m.recipient', ':user')
+            ))
+            ->setParameter('user', $user)
             ->getQuery()
-            ->getResult()
-            ;
+            ->getResult();
     }
 
-    /**
-     * @return Message[] Returns an array of Message objects
-     */
-    public function findUnreadMessages(User $user): array
+    public function findUnreadMessagesInConversation(User $user, User $otherUser): array
     {
         return $this->createQueryBuilder('m')
             ->where('m.recipient = :user')
+            ->andWhere('m.sender = :otherUser')
             ->andWhere('m.isRead = :isRead')
             ->setParameter('user', $user)
+            ->setParameter('otherUser', $otherUser)
             ->setParameter('isRead', false)
             ->getQuery()
-            ->getResult()
-            ;
+            ->getResult();
+    }
+
+    public function findConversation(User $user1, User $user2): array
+    {
+        return $this->createQueryBuilder('m')
+            ->where('(m.sender = :user1 AND m.recipient = :user2) OR (m.sender = :user2 AND m.recipient = :user1)')
+            ->setParameter('user1', $user1)
+            ->setParameter('user2', $user2)
+            ->orderBy('m.createdAt', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 }
